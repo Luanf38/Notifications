c4b6a30c0af1355f65577a80a0db81a2d347bf8a
luanf38 <luanfelipe38@hotmail.com>
2025-06-01 13:39:07 -0300

    first commit

---
+++ app/src/main/java/com/luanlima/notifications/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.luanlima.notifications
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.luanlima.notifications.ui.theme.NotificationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotificationsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    ElevatedButton(
+        modifier = modifier,
+        onClick = {}
+
+    ) {
+        Text(text = "Show notification")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotificationsTheme {
+        Greeting()
+    }
+}

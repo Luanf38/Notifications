572cc75322e3c6778753ded323a6cf7f1a9b8380
luanf38 <luanfelipe38@hotmail.com>
2025-06-02 20:47:07 -0300

    Notification channel

---
+++ app/src/main/java/com/luanlima/notifications/data/notiication/NotificationChannel.kt
@@ -0,0 +1,9 @@
+package com.luanlima.notifications.data.notiication
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build
+import androidx.core.content.ContextCompat.getSystemService
+
